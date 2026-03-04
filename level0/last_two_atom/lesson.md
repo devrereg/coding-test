aab2ca99670e73a7b0472bb337f11f3c4d356678
devrereg <devrereg@gmail.com>
2026-03-04 22:47:58 +0900

    level0: 마지막 두 원소 (보충)

--- level0/last_two_atom/lesson.md
+++ level0/last_two_atom/lesson.md
@@ -9,3 +9,14 @@ result = nums.append(4)
 print(result)   # None
 print(nums)     # [1, 2, 3, 4]
 ```
+
+<다른 풀이>   
+```python
+def solution(num_list):
+    n1, n2 = num_list[-1], num_list[-2]
+    if n1 > n2:
+        num_list.append(n1 - n2)
+    else:
+        num_list.append(n1 * 2)
+    return num_list
+```
--- level0/last_two_atom/solution.py
+++ level0/last_two_atom/solution.py
@@ -8,6 +8,14 @@ def solution(num_list):
     return num_list
 
 
+def solution2(num_list):
+    n1, n2 = num_list[-1] , num_list[-2]
+    num_list.append(n1 - n2) if n1 - n2 > 0 else num_list.append(2*n1)
+    return num_list
+
 if __name__ == "__main__":
     print(solution([2,1,6]))
     print(solution([5,2,1,7,5]))
+
+    print(solution2([2,1,6]))
+    print(solution2([5,2,1,7,5]))

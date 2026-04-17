3b127ccdc88e9b120bfdca053f0ae5e8e37ff3f1
devrereg <devrereg@gmail.com>
2026-04-17 19:42:04 +0900

    무작위로 k개의 수 뽑기

---
+++ level0/무작위로 K개의 수 뽑기/lesson.md
@@ -0,0 +1,5 @@
+set 활용하기
+
+배열에 배열을 함치기 extends
+
+dict.fromkeys(arr) => set과 같다.
---
+++ level0/무작위로 K개의 수 뽑기/solution.py
@@ -0,0 +1,24 @@
+def solution(arr, k):
+    answer = []
+    for i, num in enumerate(arr):
+        if not num in answer:
+            answer.append(num)
+            if len(answer) == k: break
+        
+    if len(answer) < k:
+        answer += [-1]*(k-len(answer))
+    return answer
+
+# 다른풀이
+def solution2(arr, k):
+    answer = list(set(arr))[:k]
+    if len(answer) < k:
+        answer += [-1]*(k - len(answer))
+        
+    return answer
+
+# 다른풀이
+def solution3(arr, k):
+    res = list(dict.fromkeys(arr))
+    res.extend([-1] * max(0, k - len(res))
+    return res[:k]
